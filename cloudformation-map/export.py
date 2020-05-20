c4322500776eeb0427b6c466a8fd991f491128df
Andrew Hynes <ahynes@rhythmictech.com>
2020-05-20 13:23:14 -0400

    changed to more useful code

--- cloudformation-map/export.py
+++ cloudformation-map/export.py
@@ -2,13 +2,26 @@
 
 import pprint
 import boto3
+import argparse
 from string import Template
 
+# argparse stuff
+parser = argparse.ArgumentParser()
+parser.add_argument("--region", help="specify an aws region")
+args = parser.parse_args()
+
+if args.region:
+    # use a different region
+    cf = boto3.resource('cloudformation', region_name=args.region)
+else:
+    # falling back to default region 
+    cf = boto3.resource('cloudformation')
+
 # setup some generic stuff, boto object, pprint
-cf = boto3.resource("cloudformation")
+
 pp = pprint.PrettyPrinter(indent=4)
 
 # takes a parameter name and returns the value
 
 for stack in cf.stacks.all():
-    print(stack)
+        print(stack.name)
